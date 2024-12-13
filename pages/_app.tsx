523cf27d2d3a1bc83795d567113c6812171296e0
nadanazeer11 <nadanazeer11@gmail.com>
2024-12-13 15:07:35 +0200

    push

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,6 @@
-import { Provider } from 'react-redux';
-import store from '@/store';
 
 export default function MyApp({ Component, pageProps }: any) {
     return (
-        <Provider store={store}>
-            <Component {...pageProps} />
-        </Provider>
+        <Component {...pageProps} />
     );
 }
