907f81ccc413b8a320e0729452f32894b40eb920
nadanzeer11 <nadanazeer11@gmail.com>
2025-03-05 15:19:59 +0200

    remove paymob

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,12 @@ import React from "react";
 
 import HowItWorks from "@/components/Features/LandingPageComponents/HowItWorks";
 import MawsouqBenefits from "@/components/Features/LandingPageComponents/MawsouqBenefits";
-import ContactUs from "@/components/Features/LandingPageComponents/ContactUs";
 import MSAnimatedDiv from "@/components/Shared/MSAnimated/MSAnimatedDiv";
 import FooterSection from "@/components/Features/LandingPageComponents/FooterSection";
 import Navbar from "@/components/Shared/MSNavBar";
 import HeroBanner from "@/components/Features/LandingPageComponents/HeroBanner";
 import LandingPageLayout from "@/layouts/LandingPageLayout";
-import PaymobFooter from "@/components/Features/LandingPageComponents/PaymobFooter";
-import AfterState from "@/components/Features/LandingPageComponents/AfterState";
 import styled from "styled-components";
-import { media } from "@/helpers/mediaQueryHelper";
 import CTA from "@/components/Features/LandingPageComponents/CTA";
 
 const LandingPage = () => {
@@ -35,7 +31,9 @@ const LandingPage = () => {
 				</MSAnimatedDiv>
 
 				<MSAnimatedDiv>
-					<PaymobFooter />
+					<FooterSection />
+
+					{/* <PaymobFooter /> */}
 					{/* <ContactUs /> */}
 				</MSAnimatedDiv>
 				<FooterSection />
