24bf123645fad181d6829e1947868f73b68b08dd
nadanzeer11 <nadanazeer11@gmail.com>
2025-04-14 20:33:02 +0200

    truust section improvmne

--- components/Features/LandingPageComponents/StartSellingSection/types.ts
+++ components/Features/LandingPageComponents/StartSellingSection/types.ts
@@ -2,14 +2,14 @@ export const startSellingEn = {
 	title: "Start  Your Transaction in Less Than 60 Seconds",
 	description:
 		"Your payments are secured. Increase sales and build client trust.",
-	placeholderProduct: "I’m selling a Samsung Smart TV 42 inch",
+	placeholderProduct: "I’m selling ",
 	placeholderPrice: "Price",
 	cta: "Create Transaction",
 };
 export const startSellingAr = {
 	title: "ابدأ أول عملية بيع في أقل من 60 ثانية",
 	description: "معاملاتك محمية تماماً، زوّد مبيعاتك، واكسب ثقة عملائك",
-	placeholderProduct: "أنا أبيع تلفاز سامسونج سمارت ٤٢ بوصة",
+	placeholderProduct: "أنا أبيع",
 	placeholderPrice: "السعر",
 	cta: "أنشئ معاملة",
 };
